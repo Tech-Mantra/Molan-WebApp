df5c4de274f5807e862f9d9753834d43f5b68458
Progyan Bhattacharya <progyanb@acm.org>
2018-02-10 11:40:26 +0530

    React: Setup Initial Development
    Created Build structure, index.html and app/index.js. Import bootstrap v4.0, Font Awesome v4.7.0 and jQuery v3.3.1
    
    Signed-off-by: Progyan Bhattacharya <progyanb@acm.org>

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,28 @@
-const webpack = require('webpack'),
-      path    = require('path'),
+const webpack = require("webpack"),
+      path    = require("path"),
       env     = process.env,
-      PACKAGE = require('./package.json'),
-      BUILD_DIR = path.resolve(__dirname, 'js'),
-      PUBLIC_DIR = path.resolve(__dirname),
-      APP_DIR = path.resolve(__dirname, 'src');
+      PACKAGE = require("./package.json"),
+      BUILD_DIR = path.resolve(__dirname, "assets/js"),
+      APP_DIR = path.resolve(__dirname, "app");
 
-let banner = PACKAGE.name.toString() + ' - ' + PACKAGE.version.toString()
-            + ' | ' + '(C) ' + new Date().getFullYear().toString() + ', '
-            + PACKAGE.author.toString() + ' | ' + PACKAGE.license.toString()
+let banner = PACKAGE.name.toString() + " - " + PACKAGE.version.toString()
+            + " | " + "(C) " + new Date().getFullYear().toString() + ", "
+            + PACKAGE.author.toString() + " | " + PACKAGE.license.toString()
 
 module.exports = {
-    entry: [ APP_DIR + '/index.js' ],
+    entry: [ APP_DIR + "/index.js" ],
     output: {
         path: BUILD_DIR,
-        publicPath: '/',
-        filename: 'bundle.js'
+        publicPath: "/",
+        filename: "bundle.js"
     },
     module: {
         loaders: [
             {
                 exclude: /node_modules/,
-                loader: 'babel',
+                loader: "babel-loader",
                 query: {
-                    presets: ['react', 'es2015', 'stage-1', 'react-hmre']
+                    presets: ["react", "es2015", "react-hmre"]
                 }
             },
             {
@@ -32,64 +31,22 @@ module.exports = {
             },
             {
                test: /\.(jpe?g|gif|png)$/,
-               loader: 'file-loader?emitFile=false&name=[path][name].[ext]'
+               loader: "file-loader?emitFile=false&name=[path][name].[ext]"
             }
         ],
-        rules: [
-            {
-              'test': /\.js$/,
-              'use': 'babel',
-              'exclude': /node_modules/,
-              'options': {
-                 'plugins': [],
-                 'presets': [['env', {
-                    'modules': false,
-                    'targets': { 'node': 6 }
-                  }]]
-              }
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
     },
     resolve: {
-        extensions: ['', '.js', '.jsx']
+        extensions: [".js", ".jsx"]
     },
     node: {
-        net: 'empty',
-        dns: 'empty'
+        net: "empty",
+        dns: "empty"
     },
-    plugins: env.NODE_ENV !== 'developement' ? [
-        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true,
-            compress: {
-                sequences: true,
-                dead_code: true,
-                conditionals: true,
-                booleans: true,
-                unused: true,
-                if_return: true,
-                join_vars: true,
-                drop_console: true,
-                warnings: false
-            },
-            mangle: {
-                except: ['$super', '$', 'exports', 'require']
-            },
-            output: {
-                comments: false
-            }
-        }),
-        new webpack.BannerPlugin(banner, { entryOnly: true })
-    ] : [],
+    plugins: [ new webpack.BannerPlugin(banner) ],
     devServer: {
         inline: true,
         historyApiFallback: true,
-        contentBase: './'
+        contentBase: "./"
     }
 };
 
